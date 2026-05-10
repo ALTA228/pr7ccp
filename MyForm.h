c31df50138b31ea9cb94b8591ff384500057fdbe
ALTA228 <pohomov38@gmail.com>
2026-05-10 17:22:03 +0300

    Add project files.

---
+++ MyForm.h
@@ -0,0 +1,134 @@
+#pragma once
+
+namespace pr7 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Label^ label1;
+	protected:
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::Button^ button1;
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(133, 104);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(56, 13);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"Введіть х:";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(275, 196);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(59, 13);
+			this->label2->TabIndex = 1;
+			this->label2->Text = L"Результат";
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(240, 97);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(100, 20);
+			this->textBox1->TabIndex = 2;
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(127, 186);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->TabIndex = 3;
+			this->button1->Text = L"Обчислити";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(898, 452);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		try {
+			double x = System::Convert::ToDouble(textBox1->Text);
+			double y;
+			double pi = 3.1415926535;
+
+			if (x < 5) {
+				y = (pi * Math::Cos(x)) / Math::Sqrt(5 - x);
+			}
+			else {
+				y = 3 + Math::Sin(5 - x);
+			}
+
+			label2->Text = "Результат y = " + y.ToString("F4");
+		}
+		catch (System::Exception^ ex) {
+			MessageBox::Show("Будь ласка, введіть коректне числове значення x.", "Помилка введення");
+		}
+	}
+	};
+
+}
